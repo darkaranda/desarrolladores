4f0fb337e68e52b52e5ada4dea7b8992171f1518
Juan Sosa <user.nuage@gmail.com>
2017-12-07 21:01:22 -0300

    Family added

--- programa.py
+++ programa.py
@@ -2,7 +2,11 @@ name = "Juan Carlos"
 surname = "Sosa Peña"
 age = 35
 address = "Chile"
+father = "Luis"
+mother = "Maria"
 print name
 print surname
 print address
 print age
+print father
+print mother
