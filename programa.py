16da91027c3c65126198be362e39ce368d054619
Juan Sosa <user.nuage@gmail.com>
2017-12-07 21:02:25 -0300

    Brother and Sister Added

--- programa.py
+++ programa.py
@@ -4,9 +4,13 @@ age = 35
 address = "Chile"
 father = "Luis"
 mother = "Maria"
+brother = "Andres"
+sister = "Lucia"
 print name
 print surname
 print address
 print age
 print father
 print mother
+print brother
+print sister
