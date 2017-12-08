c5235c66533eb0885ef65337cc48b74a9663f98b
Juan Sosa <user.nuage@gmail.com>
2017-12-07 21:28:56 -0300

    Age removed

--- programa.py
+++ programa.py
@@ -1,6 +1,5 @@
 name = "Juan Carlos"
 surname = "Sosa Peña"
-age = 35
 address = "Chile"
 father = "Luis"
 mother = "Maria"
@@ -9,7 +8,6 @@ sister = "Lucia"
 print name
 print surname
 print address
-print age
 print father
 print mother
 print brother
